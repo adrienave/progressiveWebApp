7336d7c42e901634290b0915bdcd4b34d1d0910d
adrien <adrien.avenia@gmail.com>
2019-11-20 11:32:00 +0100

    Improved ServiceWorker

--- js/app.js
+++ js/app.js
@@ -327,7 +327,6 @@ document.addEventListener("DOMContentLoaded", function(_e) {
     window.onresize(); // called to initially set the height.
     
     if('serviceWorker' in navigator) {
-		console.log("Test");
 		navigator.serviceWorker.register('./js/serviceWorker.js');
 	};
 
